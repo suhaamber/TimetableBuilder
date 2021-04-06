7aa09716b6c12fbc13dd8b844ad735198a3b0ee0
Suha Amber <suhaamber@gmail.com>
2021-04-06 19:52:10 +0400

    TimetableBuilder reads file successfully

--- TimetableBuilder.java
+++ TimetableBuilder.java
@@ -19,14 +19,48 @@ public class TimetableBuilder {
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                if (line == "#") {
+
+                if (line.equals("#")) {
+                    Course tempCourse = new Course(); 
                     // read course number
-                    // read course name
-                    // read course inst
-                    // read num of lecs
-                    // read hours
-                } else {
                     line = scanner.nextLine(); 
+                    tempCourse.setCourseNumber(line);
+                    // read course name                    
+                    line = scanner.nextLine(); 
+                    tempCourse.setCourseNumber(line);
+                    // read course inst
+                    line = scanner.nextLine();
+                    tempCourse.setCourseInstructor(line);
+                    // read num of lecs
+                    line = scanner.nextLine(); 
+                    String[] lectures = line.trim().split("\\s+");
+                    int numberOfLectures = Integer.parseInt(lectures[0]);
+                    int numberOfTutorials = Integer.parseInt(lectures[1]);
+                    int numberOfLabs = Integer.parseInt(lectures[2]);
+                    tempCourse.setNumberOfLectures(numberOfLectures, numberOfLabs, numberOfTutorials);
+                    // read lecture hours
+                    
+                    for(int i = 0; i < numberOfLectures; i++)
+                    {
+                        lectures = scanner.nextLine().trim().split("\\s+");
+                        Hour tempHour = new Hour(lectures[0].toCharArray(), Integer.parseInt(lectures[1]));
+                        tempCourse.addLectureHour(tempHour); 
+                    }
+                    //read lab hours
+                    for(int i = 0; i < numberOfLabs; i++)
+                    {
+                        lectures = scanner.nextLine().trim().split("\\s+");
+                        Hour tempHour = new Hour(lectures[0].toCharArray(), Integer.parseInt(lectures[1]));
+                        tempCourse.addLabHour(tempHour); 
+                    }
+                    //read tutorial hours
+                    for(int i = 0; i < numberOfTutorials; i++)
+                    {
+                        lectures = scanner.nextLine().trim().split("\\s+");
+                        Hour tempHour = new Hour(lectures[0].toCharArray(), Integer.parseInt(lectures[1]));
+                        tempCourse.addTutorialHour(tempHour); 
+                    }
+                } else {
                     String[] splitString = line.trim().split("\\s+");
                     timetableBuilder.setSemester(Integer.parseInt(splitString[0]));
                     timetableBuilder.setYear(Integer.parseInt(splitString[1]));
@@ -36,8 +70,7 @@ public class TimetableBuilder {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        // extract year and sem
-        // extract courses
+
         // create a 2D timetable like array
         // fill array with course number
         // attempt to randomly find a free hour
