df5b4d979201216232ea27cf4534605afaf18fb6
Suha Amber <suhaamber@gmail.com>
2021-04-06 17:35:00 +0400

    Added setters for Course

--- Course.java
+++ Course.java
@@ -10,4 +10,29 @@ public class Course {
     ArrayList<Hour> labHours = new ArrayList<Hour>(); 
     ArrayList<Hour> tutorialHours = new ArrayList<Hour>(); 
 
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setNumberOfLectures(int lecture, int lab, int tutorial) {
+        this.lecture = lecture;
+        this.lab = lab; 
+        this.tutorial = tutorial; 
+    }
+
+    public void addLectureHour(Hour hour){
+        lectureHours.add(hour);
+    }
+
+    public void addLabHour(Hour hour){
+        labHours.add(hour);
+    }
+
+    public void addTutorialHour(Hour hour){
+        tutorialHours.add(hour); 
+    }
 }
